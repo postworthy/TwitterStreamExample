829a2d8098df1990661d5f5464b2d0192c4800d8
Landon Key <jolkey@JOLKEY15.southernco.com>
2016-02-08 09:55:05 -0600

    First Commit

---
+++ TwitterStreamExample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterStreamExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var request = OAuth.CreateSignedRequest(new Uri("https://stream.twitter.com/1.1/statuses/filter.json?delimited=length&track=twitter"));
+            using (var stream = request.GetResponse().GetResponseStream())
+            using (var reader = new StreamReader(stream))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var data = reader.ReadLine();
+                    if (data[0] == '{')
+                    {
+                        Console.WriteLine(data);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
